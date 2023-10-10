59f2901e319f79878dddd79d3f27334c54cd5058
OctavioM3 <octaviom3@nycstudents.net>
2023-10-10 09:29:37 -0400

    hjghjhgj

---
+++ object.js
@@ -0,0 +1,13 @@
+const people = [
+    {
+        Name: 'Stevon',
+        nmuber: [1,2,3,4]
+    },
+    {
+        Name: 'Chris',
+        number: [5,6,7,8]
+    },
+];
+
+people.forEach((people) => console.log(people.Name));
+people.forEach((people) => console.log(people.number[number.length]));
