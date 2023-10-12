026756c3fb0a8ebe92f0bafa2f7536c7d14434dd
OctavioM3 <octaviom3@nycstudents.net>
2023-10-12 09:23:47 -0400

    lkk;j;u;lk

--- object.js
+++ object.js
@@ -1,3 +1,4 @@
+
 const people = [
     {
         Name: 'Stevon',
@@ -5,19 +6,19 @@ const people = [
     },
     {
         Name: 'Chris',
-        number: [5,6,7,8]
+        numbers: [5,6,7,8]
     },
 ];
 
 people.forEach((people) => console.log(people.Name));
 people.forEach((element) => console.log(element.number));
 
-const logElement = (element, index) => {
-    console.log(`a[${index}] = ${element}`);
-  };
-  
-  [number].forEach(logElement);
-  // Logs:
-  // a[0] = 2
-  // a[1] = 5
-  // a[3] = 9
+people.forEach((number) => {
+    console.log(number)
+});
+
+const logArrayElements = (element) => {
+    console.log(`${element}`);
+};
+
+[people.number].forEach(logArrayElements);
